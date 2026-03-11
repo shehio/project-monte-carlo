3ce343d86b64cc9a929647f8439fd0a18c9dbee0
shehio <shehabyasser@gmail.com>
2026-03-11 09:20:00 +0000

    eslint config with typescript-eslint plugin

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ['assets/js/**/*.ts', 'tests/**/*.ts'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-non-null-assertion': 'off',
+    },
+  },
+  {
+    ignores: ['dist/', 'public/', 'static/', 'node_modules/'],
+  },
+);
